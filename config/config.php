c0cff0ac43ca60e6e5514504348a5d5a73f347d2
sridharan <sridharan01234@gmail.com>
2024-05-28 10:23:15 +0530

    Subject: Configure database connection

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * This file contains db constants
+ *
+ * Author : sridharan
+ * Email : sridharan01234@gmail.com
+ * Last modified : 8/5/2024
+ */
+
+define("host", "localhost");
+define("user", "root");
+define("pass", "password");
+define("dbname", "address_book");
