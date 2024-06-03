ac658aed625629ef737276200ac3a9d251e1e762
sridharan <sridharan01234@gmail.com>
2024-06-03 15:35:09 +0530

    feat: Implement User Management API Endpoints
    This commit introduces a new UserController class providing API endpoints for essential user management operations.
    
    It enables functionalities for user profile retrieval, updates, password changes, and account deletion, enhancing the system's ability to handle user data securely and efficiently.

---
+++ controller/UserController.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * User Controller
+ *
+ * @author Sridharan sridharan01234@gmail.com
+ * Last Modified : 03-06-2024
+ */
+
+require "BaseController.php";
+require './model/UserModel.php';
+
+class UserController extends BaseController
+{
+    /**
+     * User Model
+     *
+     * @var UserModel
+     */
+    private $userModel;
+
+    /**
+     * Constructor
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->userModel = new UserModel();
+    }
+
+    /**
+     * Delete User
+     *
+     * @return void
+     */
+    public function userDelete(): void
+    {
+        $data = $this->decodeRequest();
+        $this->userModel->deleteUser($data['user_id']);
+
+        echo json_encode(['success' => true]);
+        exit;
+    }
+
+    /**
+     * Get User
+     *
+     * @return void
+     */
+    public function userProfile(): void
+    {
+        $data = $this->decodeRequest();
+        $user = $this->userModel->getUser($data['user_id']);
+
+        echo json_encode($user);
+        exit;
+    }
+
+    /**
+     * Update User
+     *
+     * @return void
+     */
+    public function userUpdate(): void
+    {
+        $data = $this->decodeRequest();
+        $details = [];
+
+        // Iterate through the desired keys
+        $keys = ['name', 'email', 'gender', 'dob', 'phone', 'address'];
+
+        foreach ($keys as $key) {
+            if (isset($data[$key])) {
+                $details[$key] = $data[$key];
+            }
+        }
+
+        // Add the 'updated_at' key regardless
+        $details['updated_at'] = date('Y-m-d H:i:s');
+        if ($this->userModel->updateUser($data['user_id'], $details)) {
+            echo json_encode(['success' => true]);
+            exit;
+        } else {
+            echo json_encode(['success' => false]);
+            exit;
+        }
+    }
+
+    /**
+     * User change password
+     *
+     * @return void
+     */
+    public function userChangePassword(): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            http_response_code(405);
+            echo json_encode(['error' => 'Method Not Allowed']);
+            exit;
+        }
+
+        $data = $this->decodeRequest();
+        $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+        $this->userModel->updateUser($data['user_id'], $data['password']);
+
+        echo json_encode(['success' => true]);
+        exit;
+    }
+}
