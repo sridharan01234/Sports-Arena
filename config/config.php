88ea7b004c2a811b13ed9658156ae2a3fd41dc68
gomathi.thiru <gomathi.thiru@aspiresys.com>
2024-06-05 10:37:53 +0530

    db config changed

--- config/config.php
+++ config/config.php
@@ -12,18 +12,3 @@ define("host", "localhost");
 define("user", "root");
 define("pass", "password");
 define("dbname", "sports_arena");
-
-<?php
-
-/**
- * This file contains db constants
- *
- * Author : sridharan
- * Email : sridharan01234@gmail.com
- * Last modified : 8/5/2024
- */
-
-define("host", "localhost");   //172.24.220.187
-define("user", "root");
-define("pass", "");
-define("dbname", "sports_arena");
