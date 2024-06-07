ec542b65bb1c9ede63be475a77e39f8a3460788b
sridharan <sridharan01234@gmail.com>
2024-06-07 14:04:34 +0530

    feat: Implement ProductModel for database interactions
    Introduces the `ProductModel` class responsible for handling all database operations related to products. This includes retrieving product details, images, and available sizes, streamlining data access for product-related functionality.

---
+++ model/ProductModel.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ *
+ */
+
+require "./database/Database.php";
+
+class ProductModel extends Database
+{
+    private $db;
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    /**
+     * Get all products
+     *
+     * @return array
+     */
+    public function get_all_products()
+    {
+        return $this->db->getAll('products', [], []);
+    }
+
+    /**
+     * Get product by id
+     *
+     * @param int $id
+     *
+     * @return object | bool
+     */
+    public function get_product(int $id): object | bool
+    {
+        return $this->db->get('products', ['product_id' => $id], []);
+    }
+
+    /**
+     * Get product images by product id
+     *
+     * @param int $id
+     *
+     * @return array
+     */
+    public function get_product_images(int $id): array
+    {
+        return $this->db->getAll('product_images', ['product_id' => $id], ['imageUrl']);
+    }
+
+    /**
+     * Get product size
+     *
+     * @param int $id
+     *
+     * @return array
+     */
+    public function get_product_size(int $id): array
+    {
+        return $this->db->getAll('product_size', ['product_id' => $id], ['size']);
+    }
+}
