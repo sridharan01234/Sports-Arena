d28f54d4c97c28d7d48e346ff5c682b2731d098d
sridharan <sridharan01234@gmail.com>
2024-06-11 11:21:56 +0530

    feat: Implement cart management endpoints
    This commit introduces API endpoints for managing cart items, enabling functionalities like adding products to a cart, retrieving cart contents, and removing items from the cart. These endpoints will serve as the foundation for cart interactions on the frontend.

--- controller/CartController.php
+++ controller/CartController.php
@@ -1,7 +1,9 @@
 <?php
 
 /**
- *
+ * CartController class
+ * 
+ * @author Sridharan
  */
 
 require './model/CartModel.php';
@@ -17,11 +19,66 @@ class CartController extends BaseController
     }
 
     /**
-     * Update cart
+     * Update cart items
+     * 
+     * @return void
      */
-    public function updateCart()
+    public function updateCart(): void           
     {
         $data = $this->decodeRequest();
         $this->cartModel->add($data);
+        echo json_encode(
+            [
+                'status' => 'success',
+                'data' => 'Product added to cart',
+            ]
+        );
+        exit;
+    }
+
+    /**
+     * Get cart items
+     * 
+     * @return void
+     */
+    public function getCart(): void
+    {
+        $data = $this->cartModel->getCart();
+        foreach ($data as $key => $product) {
+            $data[] = $this->correctNaming($product);
+        }
+        echo json_encode(
+            [
+                'status' => 'success',
+                'data' => $data,
+            ]
+        );
+        exit;
+    }
+
+    /**
+     * Remove cart item
+     * 
+     * @return void
+     */
+    public function removeCart(): void
+    {
+        $data = $this->decodeRequest();
+        if ($this->cartModel->removeCart($data['productId'])) {
+            echo json_encode(
+                [
+                    'status' => 'success',
+                    'data' => 'Product removed from cart',
+                ]
+            );
+        } else {
+            echo json_encode(
+                [
+                    'status' => 'error',
+                    'data' => 'Product not removed from cart',
+                ]
+            );
+        }
+        exit;
     }
 }
